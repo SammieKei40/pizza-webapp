a59e4e9d1275abe356e0f4b74a78e77c744b8815
SammieKei40 <sammiekei9@gmail.com>
2023-06-20 12:07:32 +0100

    React is interesting

--- src/App.jsx
+++ src/App.jsx
@@ -52,6 +52,40 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript"
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components"
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX"
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props"
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook"
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element"
+  }
+];
+
 function App() {
   
   return (
@@ -61,6 +95,7 @@ function App() {
         <Menu />
         <Steps/>
         <Counter />
+        <FlashCards />
         <Footer />
       </div>
     </>
@@ -221,7 +256,22 @@ function Counter(){
   )
 }
 
+function FlashCards(){
+  const [selectedIds, setSelectedIds] = useState(null)
 
+  return (
+    <>
+    <h1>Flash Cards</h1>
+      <div className='flashcards'>
+        {questions.map((question) => (<div key={question.id} 
+        onClick={() => setSelectedIds(question.id !== selectedIds ? question.id : null)}
+        className={question.id === selectedIds ? "selected" : ""}>
+          <h1>{question.id === selectedIds ? question.answer : question.question}</h1>
+          </div>))}
+      </div>
+    </>
+  )
+}
 
 
 
